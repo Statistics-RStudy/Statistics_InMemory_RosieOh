ab2be521cae6bef0bbe54fd7d5ea3c61cac8f78c
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-17 23:44:35 +0900

    ♻️ REFACTOR : 절대경로 파일 설정 변경

--- my_analysis/my_analysis/settings.py
+++ my_analysis/my_analysis/settings.py
@@ -10,13 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
-import os
+import os  
+from pathlib import Path  
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# Notebook 저장 디렉토리
-NOTEBOOKS_DIR = os.path.join(BASE_DIR, 'notebooks')
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 
 # Quick-start development settings - unsuitable for production
